6b55c9fe458aa6f808d4d961492debc0b289c134
TanyReye <jcreyes1531@gmail.com>
2026-08-28 00:47:39 -0600

    Clase incidencia con atributos

--- GoXela Delivery Proyecto 01/Incidencia.cs
+++ GoXela Delivery Proyecto 01/Incidencia.cs
@@ -1,4 +1,51 @@
 ﻿class Incidencia
 {
+    private string codigo;
 
+    public string Codigo
+    {
+        get { return codigo; }
+        set { codigo = value; }
+    }
+
+    private string tipo;
+
+    public string Tipo
+    {
+        get { return tipo; }
+        set { tipo = value; }
+    }
+
+    private string descripcion;
+
+    public string Descripcion
+    {
+        get { return descripcion; }
+        set { descripcion = value; }
+    }
+
+    private string fecha;
+
+    public string Fecha
+    {
+        get { return fecha; }
+        set { fecha = value; }
+    }
+
+    private string estadoIncidencia;
+
+    public string EstadoIncidencia
+    {
+        get { return estadoIncidencia; }
+        set { estadoIncidencia = value; }
+    }
+
+    public Incidencia(string codigo, string tipo, string descripcion, string fecha, string estadoIncidencia)
+    {
+        Codigo = codigo;
+        Tipo = tipo;
+        Descripcion = descripcion;
+        Fecha = fecha;
+        EstadoIncidencia = estadoIncidencia;
+    }
 }
